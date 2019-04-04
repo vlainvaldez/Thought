02b40b175276735fd7f5dc60e9437b5b1387a20f
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-04-04 10:19:35 +0800

    added ignore

---
+++ Thought/Main/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Thought
+//
+//  Created by Novare Account on 04/04/2019.
+//  Copyright © 2019 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+public final class MainVC: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Thought/Main/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Thought
+//
+//  Created by Novare Account on 04/04/2019.
+//  Copyright © 2019 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
