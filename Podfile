bfe56a1637349b8483dc848bf16610bb4c1cb594
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-04-04 10:33:08 +0800

    added SnapKit Pod and extension for add subview

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'Thought' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Thought
+  pod 'SnapKit'
+
+  target 'ThoughtTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ThoughtUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Thought/Utilities/Extensions/UIViewExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewExtension.swift
+//  Thought
+//
+//  Created by Novare Account on 04/04/2019.
+//  Copyright © 2019 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func subview(forAutoLayout subview: UIView) {
+        self.addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func subviews(forAutoLayout subviews: UIView...) {
+        self.subviews(forAutoLayout: subviews)
+    }
+    
+    func subviews(forAutoLayout subviews: [UIView]) {
+        subviews.forEach(self.subview)
+    }
+}
