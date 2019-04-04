221afddb1a7175f1a85b484ece234cf8983fa0c1
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-04-04 10:03:06 +0800

    Initial Commit

---
+++ Thought/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thought
+//
+//  Created by Novare Account on 04/04/2019.
+//  Copyright © 2019 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
